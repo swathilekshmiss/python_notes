9b90237fa76d113f5d8aec481b81eff63cef3392
swathilekshmiss <ssswathilekshmi@gmail.com>
2026-08-10 10:21:24 +0530

    index12

--- index12.py
+++ index12.py
@@ -1,11 +1,25 @@
-# a=([1,2],[3,4])
-# a[0][1]=20
-# print(a)
-# //([1, 20], [3, 4])
 
-# A = {1, 2,{3}, 4}
-# B = {3, 4, 5}
-# print(A|B)//TYPEERROR  
+# module 7 
+
+# import math
+# print(math.sqrt(25))  //5.0
 
 
-frozenset([1, 2, 3]frozenset[4,5]): "Group A",
+# from math import sqrt
+# print(sqrt(25))  //5.0  
+
+# from math import sqrt, pow
+# print(sqrt(25)) // 5.0
+# print(pow(2,3)) // 8.0 
+
+
+# import math as m
+# print(m.sqrt(25))
+
+
+# from datetime import datetime as dt
+# print(dt.now()) //2026-08-10 10:07:31.618761
+
+
+import calculator 
+print(calculator.add(5,3))
